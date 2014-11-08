c32eedb7f08132127e39f2e1542dadab4da43b5a
Endel Dreyer <endel.dreyer@gmail.com>
2014-11-07 22:13:28 -0200

    fix getting server key for phpunit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,14 +5,15 @@ class TestCase extends PHPUnit_Framework_TestCase {
 
 	public function setUp() {
 		$app = json_decode(file_get_contents('tests/app.json'), true);
-		$server_key = end(array_filter($app['keys'], function($key) {
+		$server_key = array_filter($app['keys'], function($key) {
 			return $key['type'] == 'server';
-		}));
+		});
+		$app_key = end($server_key);
 
 		$this->client = Hook\Client::configure(array(
-			'app_id' => $server_key['app_id'],
-			'key' => $server_key['key'],
-			'endpoint' => 'http://hook.dev/public/index.php/'
+			'app_id' => $app_key['app_id'],
+			'key' => $app_key['key'],
+			'endpoint' => 'http://hook.dev/index.php/'
 		));
 		parent::setUp();
 	}
